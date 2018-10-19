24b358c067b903939aa5b06436eff67de80d340c
Gabriele Ara <gabriele.ara@live.it>
2018-10-19 15:40:15 +0200

    Completed producer/consumer scenario with correct main programs

--- Labs/04 - Shared Memories/main_consumer.c
+++ Labs/04 - Shared Memories/main_consumer.c
@@ -1,49 +1,42 @@
-#include "myassert.h"   // assert_perror
+#include <stdio.h>  // printf
+#include <stdlib.h> // exit
 
-#include <stdio.h>      // printf
-#include <stdlib.h>     // exit
-
-#include <errno.h>      // errno
-
-#include <sys/ipc.h>    // shmget, shmctl
-#include <sys/shm.h>    // shmget, shmctl, shmat, shmdt
-
-#include <signal.h>     // signal
-
-#include <stdbool.h>    // bool
+#include "shmlib.h"
 
 int main(int argc, char *argv[])
 {
-    int shmkey;
-    int shmid;
-    void *shared_ptr;
-    int value;
+    struct shared_memory *shmem = shmem_get("keyfile", 'A');
+    int err;
 
-    // Using a special file to create a (possibly) unique key for the shared
-    // memory
-    shmkey = ftok("keyfile", 'A');
-    assert_perror(errno);
+    if (shmem == NULL)
+    {
+        printf("Error retrieving shared memory!\r\n");
+        return EXIT_FAILURE;
+    }
 
-    printf("The shared memory key is %d.\r\n", shmkey);
+    char c;
+    int count = 0;
 
-    // Get a previously created shared memory object
-    shmid = shmget(shmkey, sizeof(int), 0666);
-    assert_perror(errno);
+    while (shmem_get_data(shmem, &c))
+    {
+        printf("%02x\t", c);
+        ++count;
+        if(count == 8)
+        {
+            printf("\r\n");
+            count = 0;
+        }
+    }
 
-    // Attaching the shared memory to the given variable
-    shared_ptr = shmat(shmid, NULL, 0);
-    assert_perror(errno);
+    printf("\r\n");
 
-    // Copying the value within the shared memory into a local variable (without
-    // using protection)
-    value = *((int*) shared_ptr);
+    err = shmem_unget(shmem);
 
-    // Detaching the shared variable
-    shmdt(shared_ptr);
-    assert_perror(errno);
-
-    // Printing the result
-    printf("The value within the shared memory is %d.\r\n", value);
+    if (err)
+    {
+        printf("Error ungetting the shared memory!");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
--- Labs/04 - Shared Memories/main_producer.c
+++ Labs/04 - Shared Memories/main_producer.c
@@ -1,88 +1,44 @@
-#include "myassert.h"   // assert_perror
+#include <limits.h>     // max char
 
 #include <stdio.h>      // printf
 #include <stdlib.h>     // exit
 
-#include <errno.h>      // errno
-
-#include <sys/ipc.h>    // shmget, shmctl
-#include <sys/shm.h>    // shmget, shmctl, shmat, shmdt
-
-#include <signal.h>     // signal
-
-#include <stdbool.h>    // bool
-
 #include "shmlib.h"
-
-
-
-
-
-
-
-
-
-bool terminate = false;
-
-void sigint_handler(int signum)
-{
-    terminate = true;
-}
 
 int main(int argc, char* argv[])
 {
-    int shmkey;
-    int shmid;
-    void* shared_ptr;
-    int value;
+    struct shared_memory* shmem = shmem_create("keyfile", 'A');
+    int err;
 
-    // Using a special file to create a (possibly) unique key for the shared
-    // memory
-    shmkey = ftok("keyfile", 'A');
-    assert_perror(errno);
+    if (shmem == NULL)
+    {
+        printf("Error in shared memory initialization!\r\n");
+        return EXIT_FAILURE;
+    }
 
-    printf("The shared memory key is %d.\r\n", shmkey);
+    for (char c = 0; c < SCHAR_MAX; ++c)
+    {
+        shmem_put_data(shmem, c);
+        printf("%02x\t", c);
 
-    // Create the shared memory object (forcing its creation, otherwise error)
-    shmid = shmget(shmkey, sizeof(int), 0666 | IPC_CREAT | IPC_EXCL);
-    assert_perror(errno);
+        if (c % 8 == 7)
+            printf("\r\n");
+    }
 
-    // Attaching the shared memory to the given variable
-    shared_ptr = shmat(shmid, NULL, 0);
-    assert_perror(errno);
+    shmem_put_data(shmem, SCHAR_MAX);
+    printf("%02x\t", SCHAR_MAX);
 
-    // Requesting a value from command line
-    printf("Please, insert an integer value: ");
-    // flush(stdout);
+    printf("\r\n\r\n");
 
-    scanf("%d", &value);
+    shmem_end_comm(shmem);
 
-    // Writing to shared memory (without using protection)
-    *((int*) shared_ptr) = value;
+    err = shmem_destroy(shmem);
 
-    // We will wait for user input to destroy the shared memory
-    printf("Execute a ^C to terminate the program correctly...");
-    fflush(stdout);
-
-    // The easy way to use a signal handler is this, but it's sometimes
-    // considered unsafe
-    signal(SIGINT, sigint_handler);
-
-    // The hard way is the following one
-    // TODO
-
-    while(!terminate)
-        ;
-
-    printf("\r\n");
-
-    // Detaching the shared variable (not necessary if we destroy the shared
-    // memory location)
-    // shmdt(shared_ptr);
-    // assert_perror(errno);
-
-    shmctl(shmid, IPC_RMID, NULL);
-    assert_perror(errno);
+    if (err)
+    {
+        printf("Error destroying the shared memory!");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
